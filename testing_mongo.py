f612f39f2c4e5636e866e22ef4e00a14f2da58dd
Christina Lin <35711060+intuitionally@users.noreply.github.com>
2019-04-19 20:37:10 -0400

    testing mongo stuff

---
+++ testing_mongo.py
@@ -0,0 +1,29 @@
+import pymongo
+from flask import Flask, request
+import json
+from bson import json_util
+from bson.objectid import ObjectId
+from pymongo import Connection
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+db = client.datab # database
+collection = db.creds # collection
+collection.insert_many(
+		{"username": "admin",
+		"password": "admin"},
+		{"username": "alice",
+		"password": "dogsRcool"},
+		{"username": "bob",
+		"password": "Baseb@ll!"}
+)
+
+connection = Connection('localhost', 27017)
+db_1 = client.datab
+
+def toJson(data):
+	return json.dumps(data, default=json_util.default)
+
+@app.route('/authenticate/<username>/<password>', methods=['GET'])
+def authenticate(username, password):
+	
