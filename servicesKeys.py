a0fb45d582bdcfdc377d5744970b51410eb93596
Ben <bshaker@vt.edu>
2019-05-11 17:18:12 -0400

    Update servicesKeys.py

--- servicesKeys.py
+++ servicesKeys.py
@@ -2,4 +2,4 @@
 # servicesKeys.py
 
 canvasCourseID = "83639"
-canvasAPIKey = "4511~mL9Tn2uuMnJI8e7oFbECIjDXkFXjwnKHWkKgpLw2nHSAJP6nB7P8Y8yschb7j2W8"
+canvasAPIKey = "4511~mL9Tn2uuMnJI8e7oFbECIjDXkFXjwnKHWkKgpLw2nHSAJP6nB7P8Y8yschb7j2W8" # this key has been deactivated
